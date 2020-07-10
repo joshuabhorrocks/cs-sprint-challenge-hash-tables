663c14837229c1e62fe39b62725c45b08a71defe
Joshua Horrocks <joshuabhorrocks@gmail.com>
2020-07-10 10:50:56 -0600

    Completed ex3, passes short test, long test takes too long

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -1,8 +1,23 @@
 def intersection(arrays):
-    """
-    YOUR CODE HERE
-    """
-    # Your code here
+    result = []
+    temp = []
+    count = 0
+
+    print("arrays: ", arrays)
+    for i in arrays:
+        temp += i
+
+    for x in temp:
+        count += 1
+        for y in temp:
+            if x == y:
+                count += 1
+                
+            if count >= 3:
+                result.append(x)
+        count = 0
+
+        result = list(dict.fromkeys(result))
 
     return result
 
@@ -10,8 +25,12 @@ def intersection(arrays):
 if __name__ == "__main__":
     arrays = []
 
-    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
-    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
-    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+    # arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
+    # arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
+    # arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+
+    arrays.append(list(range(4, 6)) + [1, 2, 3])
+    arrays.append(list(range(7, 9)) + [1, 2, 3])
+    arrays.append(list(range(10, 12)) + [1, 2, 3])
 
     print(intersection(arrays))
